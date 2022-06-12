782e5ea4742bfb26c6100a419cdbe87a5acbb66f
rekby <timofey.koolin@gmail.com>
2022-06-12 11:31:18 +0300

    add use cache example

---
+++ examples/simple/use_cache_params_test.go
@@ -0,0 +1,29 @@
+//go:build go1.18
+// +build go1.18
+
+package simple
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/rekby/fixenv"
+	"github.com/stretchr/testify/require"
+)
+
+// namedRandom return random number for new name args
+// but return same value for all calls with same names
+func namedRandom(e fixenv.Env, name string) int {
+	return fixenv.Cache(e, name, nil, func() (res int, err error) {
+		return rand.Int(), nil
+	})
+}
+
+func TestNamedRandom(t *testing.T) {
+	e := fixenv.NewEnv(t)
+	first := namedRandom(e, "first")
+	second := namedRandom(e, "second")
+	require.NotEqual(t, first, second)
+	require.Equal(t, first, namedRandom(e, "first"))
+	require.Equal(t, second, namedRandom(e, "second"))
+}
